bf4c8b866fc62956cdab15f2cf05d02be0c4b1ca
mrbassx <abassiddrisu9222@gmail.com>
2025-03-14 16:18:39 +0000

    gpmr

---
+++ js/javascript.js
@@ -0,0 +1,37 @@
+   
+
+
+
+
+
+
+
+   let password  = false ;
+
+   let username =  true
+
+   let ValueName =  password && username 
+
+   console.log(ValueName)
+
+   ValueName ? alert("You Have LogIn") : alert("Wrong Creadential try Again");
+
+
+ 
+
+
+
+
+    // var x = 120 ;
+    // var y = 120;
+    // var z = x + y;
+    // console.log(z)
+
+    // const x = 120 ;
+    // const y = 120;
+    // const z = x + y;
+    // console.log(z)
+// let  Salam = document.body.style;
+// Salam.backgroundColor ='green'
+// Salam.color ='red'
+// Salam.fontSize = "120px"
